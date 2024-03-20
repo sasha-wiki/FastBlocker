184e716d6623538191accaa560c6e90581962c52
nacaru-w <ignaciocr92@gmail.com>
2024-03-20 11:23:39 +0100

    Allowing morebits windows to stay in place instead of having the user window scroll up and making code prettier

--- FastBlocker.js
+++ FastBlocker.js
@@ -2,19 +2,19 @@ const namespace = mw.config.get('wgCanonicalSpecialPageName');
 let username;
 
 const listMotiveOptions = [
-	{ name: 'Vandalismo de páginas', value: '[[WP:VN|Vandalismo de páginas]]' },
-	{ name: 'Cuenta creada para vandalizar', value: '[[WP:VN|Cuenta creada para vandalizar]]' },
+    { name: 'Vandalismo de páginas', value: '[[WP:VN|Vandalismo de páginas]]' },
+    { name: 'Cuenta creada para vandalizar', value: '[[WP:VN|Cuenta creada para vandalizar]]' },
     { name: 'CPP', value: '[[WP:CPP|Cuenta con propósito particular]]' },
     { name: 'Nombre de usuario inapropiado', value: '[[WP:NU|Viola la política de nombres de usuario]]' },
-	{ name: 'Usuario títere', value: '[[WP:UT#Situaciones de prohibición|Abuso de múltiples cuentas y/o evasión de bloqueo local o global]] ' },
-	{ name: 'LTA', value: '[[Wikipedia:Abuso a largo plazo|Abuso a largo plazo (LTA)]]' },
+    { name: 'Usuario títere', value: '[[WP:UT#Situaciones de prohibición|Abuso de múltiples cuentas y/o evasión de bloqueo local o global]]' },
+    { name: 'LTA', value: '[[Wikipedia:Abuso a largo plazo|Abuso a largo plazo (LTA)]]' },
     { name: 'Spam', value: '[[WP:SPAM|Spam]]' },
     { name: 'Proxy abierto', value: '[[Wikipedia:Proxies abiertos|Proxy abierto]], [[Zombi (informática)|zombi]] o [[botnet]]' }
 ];
 
 const listDurationOptions = [
     { name: 'para siempre', value: 'never' },
-	{ name: '31 horas', value: '31 hours', default: true },
+    { name: '31 horas', value: '31 hours', default: true },
     { name: '3 días', value: '3 days' },
     { name: '1 semana', value: '1 week' },
     { name: '2 semanas', value: '2 weeks' },
@@ -59,7 +59,7 @@ function getLastBlocks(username) {
         lelimit: "3"
     };
     let api = new mw.Api().get(params);
-    let blockInfo = api.then(function (data){
+    let blockInfo = api.then(function (data) {
         console.log(data);
         let logevents = data.query.logevents;
         let newList = document.createElement('ul');
@@ -76,12 +76,12 @@ function getLastBlocks(username) {
 }
 
 function getOptions(list) {
-	let dropDownOptions = [];
-	for (let a of list) {
-		let option = { type: 'option', value: a.value, label: a.name, selected: a.default };
-		dropDownOptions.push(option);
-	}
-	return dropDownOptions;
+    let dropDownOptions = [];
+    for (let a of list) {
+        let option = { type: 'option', value: a.value, label: a.name, selected: a.default };
+        dropDownOptions.push(option);
+    }
+    return dropDownOptions;
 }
 
 function createFormWindow() {
@@ -91,8 +91,8 @@ function createFormWindow() {
     let form = new Morebits.quickForm(submitBlock);
 
     let lastBlocks = form.append({
-       type: 'field',
-       label: 'Últimos bloqueos:' 
+        type: 'field',
+        label: 'Últimos bloqueos:'
     });
 
     lastBlocks.append({
@@ -102,34 +102,34 @@ function createFormWindow() {
     });
 
     let blockOptions = form.append({
-		type: 'field',
-		label: 'Opciones:'
-	});
-
-	blockOptions.append({
-		type: 'select',
-		name: 'motive',
-		label: 'Motivo:',
-		list: getOptions(listMotiveOptions),
-		disabled: false
-	});
+        type: 'field',
+        label: 'Opciones:'
+    });
 
     blockOptions.append({
-		type: 'select',
-		name: 'time',
-		label: 'Duración:',
-		list: getOptions(listDurationOptions),
-		disabled: false
-	});
+        type: 'select',
+        name: 'motive',
+        label: 'Motivo:',
+        list: getOptions(listMotiveOptions),
+        disabled: false
+    });
+
+    blockOptions.append({
+        type: 'select',
+        name: 'time',
+        label: 'Duración:',
+        list: getOptions(listDurationOptions),
+        disabled: false
+    });
 
     form.append({
-		type: 'submit',
-		label: 'Aceptar'
-	});
+        type: 'submit',
+        label: 'Aceptar'
+    });
 
-	let result = form.render();
-	Window.setContent(result);
-	Window.display();
+    let result = form.render();
+    Window.setContent(result);
+    Window.display();
 
     getLastBlocks(username).then(function (apiLastBlocks) {
         let showLastBlocks = document.querySelector("div[name='lastBlocks'] > span.quickformDescription");
@@ -143,7 +143,7 @@ function createFormWindow() {
 
 function submitBlock(e) {
     let form = e.target;
-	let input = Morebits.quickForm.getInputData(form);
+    let input = Morebits.quickForm.getInputData(form);
     new mw.Api().postWithToken('csrf', {
         action: 'block',
         user: username,
@@ -154,18 +154,18 @@ function submitBlock(e) {
         allowusertalk: true,
         anononly: true,
         reblock: true
-    }).then(function() {
+    }).then(function () {
         mw.notify(`${username} ha sido bloqueado.`);
     });
 }
 
 function createUserToolButton() {
-    mw.hook('wikipage.content').add(function(obj) {
-        obj.find('span.mw-usertoollinks').each(function(idx, element) {
+    mw.hook('wikipage.content').add(function (obj) {
+        obj.find('span.mw-usertoollinks').each(function (idx, element) {
             $(element).contents().last().after(' · ',
-                $('<a>').attr('href', '#')
-                    .text('bloqueo rápido')  
-                    .click(function() {
+                $('<a>').attr('href', '#!')
+                    .text('bloqueo rápido')
+                    .click(function () {
                         username = getUsername($(this));
                         createFormWindow();
                     })
